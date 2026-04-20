a1669ee17e482d9e2a56540a33f700f48e25af72
appfiltrosgoby <appfiltrosgoby@gmail.com>
2026-04-20 10:41:26 -0500

    ruta img

--- public/assets/js/rewards-catalog.js
+++ public/assets/js/rewards-catalog.js
@@ -1,105 +1,101 @@
-// Catalogo editable de premios canjeables.
-// 1) Sube las imagenes a public/assets/images/rewards/
-// 2) Asegura que el nombre del archivo coincida con el campo image
-// 3) Ajusta cost para cambiar los puntos requeridos
 
 window.REWARDS_CATALOG = [
     {
         id: 'sacacorchos-destapador',
         name: 'Sacacorchos, Destapador',
         cost: 150,
-        image: 'sacacorchos.jpg',
+        image: '/assets/images/rewards/sacacorchos.jpg',
         description: 'Premio canjeable por 150 puntos.'
     },
     {
         id: 'gorra-goby',
         name: 'Gorra goby',
         cost: 150,
-        image: 'gorra.png',
+        image: '/assets/images/rewards/gorra.png',
         description: 'Premio canjeable por 150 puntos.'
     },
     {
         id: 'termo',
         name: 'Termo',
         cost: 150,
-        image: 'termo.webp',
+        image: '/assets/images/rewards/termo.webp',
         description: 'Premio canjeable por 150 puntos.'
     },
     {
         id: 'mini-altavoz-silicona',
         name: 'Mini Altavoz de Silicona',
         cost: 225,
-        image: 'bafle.png',
+        image: '/assets/images/rewards/bafle.png',
         description: 'Premio canjeable por 225 puntos.'
     },
     {
         id: 'lonchera-calentador',
         name: 'Lonchera calentador',
         cost: 450,
-        image: 'lonchera.webp',
+        image: '/assets/images/rewards/lonchera.webp',
         description: 'Premio canjeable por 450 puntos.'
     },
     {
         id: 'audifonos-inalambricos',
         name: 'Audifonos Inalambricos',
         cost: 450,
-        image: 'audifonos.webp',
+        image: '/assets/images/rewards/audifonos.webp',
         description: 'Premio canjeable por 450 puntos.'
     },
     {
         id: 'powerbank',
         name: 'Powerbank',
         cost: 450,
-        image: 'powerbank.webp',
+        image: '/assets/images/rewards/powerbank.webp',
         description: 'Premio canjeable por 450 puntos.'
     },
     {
         id: 'cafetera-electrica',
         name: 'Cafetera Electrica',
         cost: 526,
-        image: 'Cafetera.webp',
+        image: '/assets/images/rewards/Cafetera.webp',
         description: 'Premio canjeable por 526 puntos.'
     },
     {
         id: 'hidrolavadora-inalambrica',
         name: 'Hidrolavadora Inalambrica',
         cost: 526,
-        image: 'hidro.webp',
+        image: '/assets/images/rewards/hidro.webp',
         description: 'Premio canjeable por 526 puntos.'
     },
     {
         id: 'destornillador-inalambrico',
         name: 'destornillador inalambrico',
         cost: 602,
-        image: 'destornillador.webp',
+        image: '/assets/images/rewards/destornillador.webp',
         description: 'Premio canjeable por 602 puntos.'
     },
     {
         id: 'licuadora',
         name: 'Licuadora',
         cost: 1230,
-        image: 'licuadora.jpg',
+        image: '/assets/images/rewards/licuadora.jpg',
         description: 'Premio canjeable por 1230 puntos.'
     },
     {
         id: 'morral',
         name: 'Morral',
         cost: 1230,
-        image: 'morral.webp',
+        image: '/assets/images/rewards/morral.webp',
         description: 'Premio canjeable por 1230 puntos.'
     },
     {
         id: 'freidora-de-aire',
         name: 'Freidora de Aire',
         cost: 1504,
-        image: 'freidora-de-aire.jpg',
+        image: '/assets/images/rewards/freidora-de-aire.jpg',
         description: 'Premio canjeable por 1504 puntos.'
     },
     {
         id: 'taladro-inalambrico',
         name: 'taladro inalambrico',
         cost: 1504,
-        image: 'taladro.webp',
+        image: '/assets/images/rewards/taladro.webp',
         description: 'Premio canjeable por 1504 puntos.'
     }
 ];
